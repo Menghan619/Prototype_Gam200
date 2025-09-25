34ac488e88cf516d0411ba8f1e73beb11fe18e14
MELTINGPOT <gohmenghan17@gmail.com>
2025-09-25 16:46:34 +0800

    Colider now works
    hafhafijanf

--- Assets/Player Scripts/Abilities.cs
+++ Assets/Player Scripts/Abilities.cs
@@ -5,15 +5,30 @@ public class Abilities : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public Animator SlashAnimes;
+    [SerializeField] GameObject AbilityQQ;
     // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        AbilityQQ.SetActive(false);
     }
+    
+
     public void AttackQQ()
     {
         
         SlashAnimes.SetTrigger("QQ");
+        AbilityQQ.SetActive(true);
+        
 
+
+    }
+    
+    public void collideroff(GameObject Ability)
+    {
+        Ability.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
--- Assets/Player Scripts/Hitbox.cs
+++ Assets/Player Scripts/Hitbox.cs
@@ -3,14 +3,35 @@ using UnityEngine;
 public class Hitbox : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    PolygonCollider2D poly;
+    ContactFilter2D filter;
+    readonly Collider2D[] buffer = new Collider2D[32]; // reuse this
+
+    void Awake()
     {
-        
+        poly = GetComponent<PolygonCollider2D>();
+
+        // Start from no filter, then tweak if you want.
+        filter = new ContactFilter2D();
+        filter.NoFilter();
+        // Example filters:
+        // filter.useTriggers = true;                      // include triggers
+        // filter.SetLayerMask(LayerMask.GetMask("Enemy")); // only Enemy layer
     }
 
-    // Update is called once per frame
     void Update()
     {
+        int count = poly.Overlap(filter, buffer); // <-- returns INT
+        for (int i = 0; i < count; i++)
+        {
+            Collider2D hit = buffer[i];
+            if (hit == null || hit == poly) continue; // skip self, just in case
+            Debug.Log("Overlapping: " + hit.name);
+            
+        }
         
+
     }
+
 }
